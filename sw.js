4ca8bcdc4092c05cfe9093ee2add4c301dd841e4
rbiiiii <robinfoguenne@territoires-memoire.be>
2019-05-07 15:11:32 +0200

    testing service worker

---
+++ sw.js
@@ -0,0 +1,32 @@
+self.oninstall = function() {
+    caches.open('tmSw')
+    .then(function(cache) {
+        cache.addAll([
+            '/',
+            'index.html'
+        ])
+        .then(function() {
+            console.log('cached !');
+        })
+        .catch(function(err) {
+            console.log('err ', err);
+        })
+    })
+}
+
+self.onactivate = function() {
+    console.log('activated');
+}
+
+self.onfetch = function(event) {
+    event.respondWidth(
+        caches.match(event.request)
+        .then(function(response) {
+            if (response) {
+                return response;
+            } else {
+                return fetch(event.request);
+            }
+        })
+    )
+}
